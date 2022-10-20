7f719e39bca5333e03e8879f4d543283b91c5719
LE TENOUX--RACHIDI Isham <isham.le.tenoux@etu.imt-lille-douai.fr>
2022-10-20 19:46:01 +0200

    added start script

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const bookRouter = require("./routes/bookRoute")
 const mongoose = require("mongoose")
 require("dotenv").config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 
 const uri = "mongodb+srv://user:user@cluster0.ajoof.mongodb.net/bookManagement?retryWrites=true&w=majority";
 
